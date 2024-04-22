083c1e077238451d5f0081a2c7a92ef5492422e2
so0y <jenny1717@naver.com>
2024-04-22 22:51:21 +0900

    searchEdit

--- src/Search/components/SearchSession.js
+++ src/Search/components/SearchSession.js
@@ -1,61 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import '../css/SearchSession.css';
 import Map from "./Map";
-
-const Information = [
-    {
-        restaurant_id: 1,
-        name : "부곡동보리밥뷔페",
-        address_road: "경북 구미시 야은로 132 1층",
-        tel : "010-2271-4738"
-    },
-    {
-        restaurant_id: 2,
-        name : "파네룬",
-        address_road: "경북 구미시 야은로7길 47 1층, 2층",
-        tel : "070-4230-6543"
-    },
-    {
-        restaurant_id: 3,
-        name : "기와집",
-        address_road: "경북 구미시 야은로 133",
-        tel : "054-442-6007"
-    },
-    {
-        restaurant_id: 4,
-        name : "모미지",
-        address_road: "경북 구미시 부곡길 119 1층",
-        tel : "010-8938-0326"
-    },
-    {
-        restaurant_id: 5,
-        name : "삼거리메밀막국수",
-        address_road: "경북 구미시 송선로 209-3",
-        tel : "054-452-1945"
-    },
-    {
-        restaurant_id: 6,
-        name : "고향가마솥추어탕 본점",
-        address_road: "경북 구미시 야은로 128",
-        tel : "054-443-8877"
-    }
-]
+import axios from "axios";
 
 function SearchSession() {
+
+    //통신데이터저장
+    const [data, setData] = useState([]);
+    //axios 통신
+    const getData = async () => {
+        try{
+            const respone = await axios
+            .get("http://localhost:8080/restaurant/findAll");
+            setData(respone.data.content);
+        } catch (err){
+            console.log({error: err});
+        }
+    };
+
+    useEffect(() =>{
+        getData();
+    }, []);
+
+    console.log(data); //data 확인용 콘솔
+
     const [search, setSearch] = useState('');
     const getValue = (e) => {
         setSearch(e.target.value.toLowerCase())
     };
-    const serched = Information.filter((item) => 
+
+    const serched = data.filter((item) => 
         item.name.toLowerCase().includes(search));
 
     const items = serched.map(data =>{
         return(
-            <div className="restaurant-list-area">
+            <div className="restaurant-list-area" key={data.id}>
                 <div className="restaurant-name">{data.name}</div>
                 <div className="restaurant-address">{data.address_road}</div>
                 <div className="restaurant-number">{data.tel}</div>
-                <div className="restaurant-stars">별점</div>
+                <div className="restaurant-stars">별점 :{data.degree}</div>
                 <div className="restaurant-btn-area">
                     <button className="restaurant-btn">즐겨찾기</button>
                     <button className="restaurant-btn">리뷰보기</button>
@@ -64,8 +47,6 @@ function SearchSession() {
         )
     })
 
-    
-    
     return (
         <div className='SearchSession'>
            <div className="search-bar-area">
