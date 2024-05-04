6cd1c74eda2468f4fa320031937a76df152f227d
so0y <jenny1717@naver.com>
2024-05-05 00:11:13 +0900

    거리순 test

--- src/Search/components/Pagination.js
+++ src/Search/components/Pagination.js
@@ -3,13 +3,13 @@ import '../css/Pagination.css';
 
 const Pagination = ({ page, totalPages, handlePageChange }) => {
     const goToPreviousPage = () => {
-        if(page > 0){
+        if(page > 1){
             handlePageChange(page - 1);
         }
     };
 
     const goToNextPage = () => {
-        if(page < totalPages - 1){
+        if(page < totalPages){
             handlePageChange(page + 1);
         }
     };
@@ -17,13 +17,11 @@ const Pagination = ({ page, totalPages, handlePageChange }) => {
 
     return (
         <div className="pagination">
-            <button className="page-btn" onClick={goToPreviousPage} disabled={page === 0}>이전</button>
-            <span className="page">{page + 1} / {totalPages} </span>
-            <button className="page-btn" onClick={goToNextPage} disabled={page === totalPages - 1}>이전</button>
+            <button className="page-btn" onClick={goToPreviousPage} disabled={page === 1}>이전</button>
+            <span className="page">{page} / {totalPages} </span>
+            <button className="page-btn" onClick={goToNextPage} disabled={page === totalPages}>다음</button>
         </div>
     )
 }
-
-
 
 export default Pagination;
--- src/Search/components/SearchSession.js
+++ src/Search/components/SearchSession.js
@@ -12,7 +12,7 @@ function SearchSession() {
     const [search, setSearch] = useState('');
 
     /*Pagination*/
-    const [page, setPage] = useState(0); //페이지 숫자
+    const [page, setPage] = useState(1); //페이지 숫자
     const [totalPages, setTotalPages] = useState(0); //전체 데이터 수
     const [restLen, setRestLen] = useState(0); // 검색 데이터 수
 
@@ -34,10 +34,17 @@ function SearchSession() {
            setTotalPages(totalPages);
            setRestLen(numberOfElements);
 
+
         } catch (err){
             console.log({error: err});
         }
     };
+
+     //검색어 상태 업데이트
+     const getValue = (e) => {
+        setSearch(e.target.value.toLowerCase());
+        setPage(1); //검색어 변경시 첫번째 페이지로 이동
+    }   
 
     // 검색어 보내기
     const ClickSearch = async () => {
@@ -58,57 +65,101 @@ function SearchSession() {
         setPage(newPage);
     };
 
-    //검색어 상태 업데이트
-    const getValue = (e) => {
-        setSearch(e.target.value.toLowerCase());
-        setPage(0); //검색어 변경시 첫번째 페이지로 이동
-    }
+    //목록별 조회 - 거리순
+    const [lat, setLat] = useState(); // 위도
+    const [lon, setLon] = useState(); // 경도
+
+    console.log("현재 위경도: ", lat, lon);
     
-    //목록별 조회 - 별점순
-    const getStar = async () => {
+     const getDistance = async () =>{
         try{
-            const respone = await axios
-            .get("http://localhost:8080/restaurant/search/degree");
-            setRestaurant(respone.data.content);
+            const response = await axios
+            .get(`http://localhost:8080/restaurant/search/routes?startX=${lat}&startY=${lon}`);
+            const {content, totalPages, numberOfElements } = response.data; //데이터 받아오기
+            setRestaurant(content);
+            setTotalPages(totalPages);
+            setRestLen(numberOfElements);
+
+            console.log("거리순 작동");
+            console.log("거리순:", content);
+
         } catch (err){
             console.log({error: err});
         }
-        console.log("별점순 작동");
+     }
+
+    //목록별 조회 - 별점순
+    const getDegree = async () => {
+        try{
+            const response = await axios
+            .get(`http://localhost:8080/restaurant/search/degree?page=${page}`);
+            const {content, totalPages, numberOfElements } = response.data; //데이터 받아오기
+            setRestaurant(content);
+            setTotalPages(totalPages);
+            setRestLen(numberOfElements);
+
+            console.log("별점순 작동");
+            console.log(restaurant);
+
+        } catch (err){
+            console.log({error: err});
+        }
     };
+
+    // const handleStarClick = () => {
+    //     setPage(1); // 버튼 클릭 시 페이지 초기화
+    //     getDegree(); // 별점순 목록 조회 함수 호출
+    // };
+
     //목록별 조회 - 리뷰순
     const getReviw = async () => {
         try{
-            const respone = await axios
-            .get("http://localhost:8080/restaurant/search/reviews");
-            setRestaurant(respone.data.content);
+            const response = await axios
+            .get(`http://localhost:8080/restaurant/search/reviews?page=${page}`);
+            const {content, totalPages, numberOfElements } = response.data; //데이터 받아오기
+            setRestaurant(content);
+            setTotalPages(totalPages);
+            setRestLen(numberOfElements);
+            setPage(1); // 페이지 초기화
+
+            console.log("리뷰순 작동");
         } catch (err){
             console.log({error: err});
         }
-        
-        console.log("리뷰순 작동");
     };
+
     //음식점 목록
     const getOnlyRestuarant = async () => {
         try{
             const respone = await axios
-            .get("http://localhost:8080/restaurant/search/onlyrestaurants");
-            setRestaurant(respone.data.content);
+            .get(`http://localhost:8080/restaurant/search/onlyrestaurants`);
+            const {content, totalPages, numberOfElements } = respone.data; //데이터 받아오기
+            setRestaurant(content);
+            setTotalPages(totalPages);
+            setRestLen(numberOfElements);
+            setPage(1);
+
+            console.log("음식점 작동");
         } catch (err){
             console.log({error: err});
         }
-        
-        console.log("음식점 작동");
     };
+
     // 카페 목록
     const getOnlyCafe = async () => {
         try{
             const respone = await axios
-            .get("http://localhost:8080/restaurant/search/onlycafes");
-            setRestaurant(respone.data.content);
+            .get(`http://localhost:8080/restaurant/search/onlycafes`);
+            const {content, totalPages, numberOfElements } = respone.data; //데이터 받아오기
+            setRestaurant(content);
+            setTotalPages(totalPages);
+            setRestLen(numberOfElements);
+            setPage(1);
+
+            console.log("카페 작동");
         } catch (err){
             console.log({error: err});
         }
-
     };
 
     //음식점을 클릭했는지
@@ -128,6 +179,7 @@ function SearchSession() {
                 <div className="restaurant-address">{data.addressRoad}</div>
                 <div className="restaurant-number">{data.tel}</div>
                 <div className="restaurant-stars">별점 :{data.degree}</div>
+                <div className="restaurant-stars">리뷰 :{data.totalReviews}</div>
                 <div className="restaurant-btn-area">
                     <button className="restaurant-btn">즐겨찾기</button>
                     <button className="restaurant-btn">리뷰보기</button>
@@ -142,7 +194,6 @@ function SearchSession() {
                     <input
                     type="serach"
                     className="search-bar"
-                    // onChange={event => {setSearch(event.target.value)}}>
                     onChange={getValue}>
                     </input>
                     <img src={process.env.PUBLIC_URL + '/img/yellow_search.png'}
@@ -152,13 +203,15 @@ function SearchSession() {
                         />
             </div>
             <div className="search-map-area">
-            <Map restaurant={search ? restaurant : (selectRest ? [selectRest] : restaurant)} restLen={restLen}/>
+            <Map restaurant={search ? restaurant : (selectRest ? [selectRest] : restaurant)}
+                restLen={restLen}
+                setLat={setLat} setLon={setLon}/>
 
             </div>
            <div className="collect-area">
                 <div className="collect-list">
-                    <button className="list-btn">거리순</button>
-                    <button className="list-btn" onClick={getStar}>별점순</button>
+                    <button className="list-btn" onClick={getDistance}>거리순</button>
+                    <button className="list-btn" onClick={getDegree}>별점순</button>
                     <button className="list-btn" onClick={getReviw}>리뷰순</button>
                 </div>
                 <div className="collect-categori">
