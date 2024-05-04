8d71904102b6c0370c3841b2f20ce41be9b792bf
Junhyun <dole614@naver.com>
2024-05-05 00:38:42 +0900

    리뷰 수정/삭제/조회 및 영수증 제출 기능 완성

---
+++ src/Review/components/ReceiptAuth.js
@@ -0,0 +1,59 @@
+import "../css/ReceiptAuth.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "../../etc/utils/apis";
+
+function ReceiptAuth() {
+  const navigate = useNavigate();
+
+  const [receipt, setReceipt] = useState(null);
+
+  // 파일(영수증) 변경시 상태 업데이트
+  const handleReceiptChange = (e) => {
+    setReceipt(e.target.files[0]); // 파일 선택시 파일 상태 업데이트
+  };
+
+  // 파일 제출
+  const submitReceipt = () => {
+    if (receipt) {
+      const formData = new FormData();
+      formData.append("file", receipt); // 'file' 키에 receipt 추가
+
+      axios
+        .post("http://localhost:8080/api/review/receipt", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data", // 파일 전송을 위한 헤더(필수)
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          alert("영수증 제출 성공");
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("영수증 제출 실패");
+        });
+    } else {
+      alert("파일을 선택해주세요.");
+    }
+  };
+
+  return (
+    <div className="ReceiptAuth">
+      <span>영수증 인증</span>
+      <br />
+      <span>리뷰를 작성하기 전 영수증 인증을 해주세요!</span>
+      <br />
+      <input type="file" onChange={handleReceiptChange} />
+      <br />
+      <span>
+        사진 상태에 따라 정상적으로 인식이 되지 않을 수도 있으니 주의해주세요
+      </span>
+      <button className="submit-btn" onClick={submitReceipt}>
+        인증하기
+      </button>
+    </div>
+  );
+}
+
+export default ReceiptAuth;
