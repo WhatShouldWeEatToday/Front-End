38bb43fc53d12ae36570227274e3aeafaf13e116
so0y <jenny1717@naver.com>
2024-05-31 20:49:53 +0900

    채팅 오류 수정중

--- src/Main/components/Chat.js
+++ src/Main/components/Chat.js
@@ -45,57 +45,7 @@ function Chat(props) {
         }
         
     };
-    const promises = (
-        <div className="promise-area">
-                    <div className="promise-content">
-                        <div className="promise-header">오늘 뭐 먹지?</div>
-                        <div className="promise-menu">떡볶이</div>
-                        <div className="promise-alltime">
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <DatePicker
-                                        label="약속 날짜를 선택해주세요"
-                                        inputFormat={"yyyy-MM-dd"}
-                                        selected={date}
-                                        onChange={(day) => setDate(day)}
-                                        slotProps={{
-                                            textField: {
-                                                size: "small",
-                                            },
-                                        }}/>
-                                    <DemoContainer components={['TimePicker']}>
-                                        <TimePicker
-                                            label="약속 시간을 선택해주세요"
-                                            selected={time}
-                                            onChange={(selectTime) => setTime(selectTime)}
-                                            slotProps={{
-                                                textField: {
-                                                    size: "small",
-                                                },
-                                            }}/>
-                                    </DemoContainer>
-                                </LocalizationProvider>
-                        </div>
-                        <div className="promise-place">
-                            <div className="place-name">
-                                약속 장소: 
-                            </div>
-                            <input type=""
-                                className="place"
-                                onChange={(e) => {setPlace(e.target.value)}}/>
-                        </div>
-                        <div className="promise-btns">
-                            <button className="promise-btn">공지 삭제</button>
-                            <button className="promise-btn">공지 등록</button>
-                        </div>
-                        
-                    </div>
-                    <div className="chat-member">
-                        <img src={process.env.PUBLIC_URL + '/img/account.png'}
-                        className="chat-mem" alt='profile'/>
-                        <div className="chat-mem-name">임수연</div>
-                    </div>
-                </div>
-    );
+    
 
     const onPopup = () => {
         const url = 'restaurant/course';
@@ -124,7 +74,6 @@ function Chat(props) {
                     {/* <button
                      className="course-btn" onClick={sendAddress}>경로조회</button> */}
                 {/* 약속 설정 */}
-                {promises}
             </div>
         </div>
     );
