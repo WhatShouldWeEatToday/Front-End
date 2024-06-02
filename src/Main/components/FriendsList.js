9ca7f48bc1d2001bf9786e4a41cd7c9fcd971a4a
so0y <jenny1717@naver.com>
2024-06-02 15:11:32 +0900

    ChatRoom 수정중

--- src/Main/components/FriendsList.js
+++ src/Main/components/FriendsList.js
@@ -2,30 +2,25 @@ import React, { useEffect, useState } from "react";
 import '../css/FriendsList.css';
 import axios from "axios";
 
-function FriendsList({onShowCreateChat}) { 
-    //나의 친구 데이터 통신
+function FriendsList({ onShowCreateChat }) { 
+    // 나의 친구 데이터 통신
     const [myFriends, setMyFriends] = useState([]);
-    //검색 데이터 저장
+    // 검색 데이터 저장
     const [search, setSearch] = useState('');
 
-    //토큰
-    const headers =
-    {
+    // 토큰
+    const headers = {
         Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`, // localStorage에서 저장된 accessToken을 가져와서 헤더에 포함
     };
 
-    //친구목록 불러오기
+    // 친구목록 불러오기
     const getMyFriends = async () => {
-        try{
-            const respone = await axios
-            .get("http://localhost:8080/chat/friend-list",
-            {
-                headers: headers, // 헤더 설정
-            });
-            setMyFriends(respone.data);
-            console.log("받아온 친구 데이터", respone.data);
-        } catch(err){
-            console.log({error: err});
+        try {
+            const response = await axios.get("http://localhost:8080/chat/friend-list", { headers });
+            setMyFriends(response.data);
+            console.log("받아온 친구 데이터", response.data);
+        } catch (err) {
+            console.log({ error: err });
         }
     };
 
@@ -33,50 +28,48 @@ function FriendsList({onShowCreateChat}) {
         getMyFriends();
     }, []);
 
-    //친구 검색
+    // 친구 검색
     const handleSearch = (e) => {
         setSearch(e.target.value.toLowerCase());
-    }
+    };
 
-    const searchFriends = myFriends.filter(friends => 
-        friends.friendNickname.toLocaleLowerCase().includes(search)
+    const searchFriends = myFriends.filter(friend => 
+        friend.friendNickname.toLowerCase().includes(search)
     );
 
     const ClickSearch = async () => {
-        try{
-            const respone = await axios
-            .get('http://localhost:8080/chat/friend/search/');
-            const content = respone.data;
+        try {
+            const response = await axios.get('http://localhost:8080/chat/friend/search/');
+            const content = response.data;
             setSearch(content);
-        } catch (err){
-            console.log({error : err});
+        } catch (err) {
+            console.log({ error: err });
         }
-    }
+    };
 
-    //채팅방 초대할 친구 선택
+    // 채팅방 초대할 친구 선택
     const [invite, setInvite] = useState({});
-    const [selectedFriends , setSelectedFriends] = useState([]);
+    const [selectedFriends, setSelectedFriends] = useState([]);
 
     const handleInvite = (friend) => {
         setInvite(prevStatus => ({
             ...prevStatus,
-            [friend.friendLoginId] : !prevStatus[friend.friendLoginId],
+            [friend.friendLoginId]: !prevStatus[friend.friendLoginId],
         }));
-        console.log("선택된 친구",invite);
+        console.log("선택된 친구", invite);
 
         setSelectedFriends(prevSelectedFriends => {
-            if(prevSelectedFriends.some(selectedFriends => selectedFriends.friendLoginId === friend.friendLoginId)) {
-                return prevSelectedFriends.filter(selectedFriends => selectedFriends.friendLoginId !== friend.friendLoginId);
-            } else{
+            if (prevSelectedFriends.some(selectedFriend => selectedFriend.friendLoginId === friend.friendLoginId)) {
+                return prevSelectedFriends.filter(selectedFriend => selectedFriend.friendLoginId !== friend.friendLoginId);
+            } else {
                 return [...prevSelectedFriends, friend];
             }
         });
     };
 
-
-    const myFriendslist = searchFriends.map(friend => {
+    const myFriendsList = searchFriends.map(friend => {
         const isInvited = invite[friend.friendLoginId] || false;
-        return(
+        return (
             <div className="friends-list" key={friend.friendLoginId}>
                 <img src={process.env.PUBLIC_URL + '/img/account.png'}
                     className="friends-profile" alt='profile'/>    
@@ -93,10 +86,8 @@ function FriendsList({onShowCreateChat}) {
                     )}
                 </div>
             </div>
-        )
-    })
-
-
+        );
+    });
 
     return ( 
         <div className="FriendsList">
@@ -111,15 +102,15 @@ function FriendsList({onShowCreateChat}) {
                     alt='search'
                     onChange={handleSearch}
                     onClick={ClickSearch}
-                    />
+                />
             </div>
-            <div className="FriendsList-body">{myFriendslist}</div>
+            <div className="FriendsList-body">{myFriendsList}</div>
             <div className="Group" onClick={() => onShowCreateChat(selectedFriends)}>
                 <img src={process.env.PUBLIC_URL + '/img/users-group.png'}
                     className="make-group"
                     alt='group'
                 />
-                <div className="group-text">그룹만들기</div>
+                <div className="group-text">그룹 만들기</div>
             </div>
         </div>
     );
