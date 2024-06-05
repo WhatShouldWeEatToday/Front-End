293b282a4bf72e663b869da7e1efaaea792ab64d
so0y <jenny1717@naver.com>
2024-06-05 11:11:08 +0900

    경로안내수정

--- src/Main/components/FriendsRequestTab.js
+++ src/Main/components/FriendsRequestTab.js
@@ -3,28 +3,22 @@ import "../css/FriendsRequestTab.css";
 import axios from "axios";
 
 function FriendsRequestTab() {
-
-    //나의 친구 데이터 통신
     const [requestFriend, setRequestFriend] = useState([]);
-    //검색 데이터 저장
     const [search, setSearch] = useState('');
 
-    const headers = 
-    {
-        Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`, // localStorage에서 저장된 accessToken을 가져와서 헤더에 포함
+    const headers = {
+        Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
     };
 
     const getRequestFriends = async () => {
-        try{
-            const respone = await axios
-            .get("http://localhost:8080/chat/friend-add-list",
-            {
-                headers: headers, // 헤더 설정
+        try {
+            const response = await axios.get("http://localhost:8080/chat/friend-add-list", {
+                headers: headers,
             });
-            setRequestFriend(respone.data);
-            console.log(respone.data);
-        } catch(err){
-            console.log({error: err});
+            setRequestFriend(response.data);
+            console.log(response.data);
+        } catch (err) {
+            console.log({ error: err });
         }
     };
 
@@ -36,47 +30,64 @@ function FriendsRequestTab() {
         setSearch(e.target.value.toLowerCase());
     }
 
-    const searchFriends = requestFriend.filter(friends => 
+    const searchFriends = requestFriend.filter(friends =>
         friends.friendNickname.toLocaleLowerCase().includes(search)
     );
 
+    const ClickSearch = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/chat/friend/search/${search}`, {
+                headers: headers,
+            });
+            const content = response.data;
+            setRequestFriend(content);
+        } catch (err) {
+            console.log({ error: err });
+        }
+    }
+
     const reqFriendslist = searchFriends.map(friend => {
-        return(
+        return (
             <div className="friends-list" key={friend.friendshipId}>
                 <img src={process.env.PUBLIC_URL + '/img/account.png'}
-                    className="friends-profile" alt='profile'/>    
+                    className="friends-profile" alt='profile' />
                 <div className="friends-name">{friend.friendNickname}</div>
                 <img src={process.env.PUBLIC_URL + '/img/invitation.png'}
-                 className="friends-invite" alt='profile'/>    
+                    className="friends-invite" alt='profile'
+                    onClick={() => InviteFriendsList(friend.friendLoginId, friend.friendNickname)} />
             </div>
         )
     })
 
-    const ClickSearch = async () => {
-        try{
-            const respone = await axios
-            .get('http://localhost:8080/chat/friend/search/');
-            const content = respone.data;
-            setSearch(content);
-        } catch (err){
-            console.log({error : err});
+    const InviteFriendsList = async (friendId, friendNickname) => {
+        try {
+            const response = await axios.post(`http://localhost:8080/chat/friend/add/${friendId}`, {}, {
+                headers: headers
+            });
+            console.log("친구 추가 데이터", response);
+            alert(`${friendNickname} 과 친구가 되었습니다.`);
+
+            // 친구 추가 후 목록에서 해당 친구 제거
+            setRequestFriend(prevRequestFriend =>
+                prevRequestFriend.filter(friend => friend.friendLoginId !== friendId)
+            );
+        } catch (err) {
+            console.log({ error: err });
         }
     }
-
 
     return (
         <div className="FriendsRequestTab">
             <div className="Friends-search">
                 <input
-                type="search"
-                className="friends-serach-bar"
-                onChange={handleSearch}
-                value={search}/>
+                    type="search"
+                    className="friends-serach-bar"
+                    onChange={handleSearch}
+                    value={search} />
                 <img src={process.env.PUBLIC_URL + '/img/yellow_search.png'}
                     className="search_icon"
                     alt='search'
-                    onChange={handleSearch}
-                    onClick={ClickSearch}/>
+                    onClick={ClickSearch} />
             </div>
             <div className="FriendsList-body">{reqFriendslist}</div>
         </div>
